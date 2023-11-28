e66f65f107eae531d229a438eec5bde92d1dad1a
DimasRidhwanaS <dimsridhwana@gmail.com>
2023-11-28 12:00:53 +0700

    Another Attempts

--- EL3111_05_20231124_13221076/Attempt/register/register.vhd
+++ EL3111_05_20231124_13221076/Attempt/register/register.vhd
@@ -4,8 +4,8 @@ use IEEE.numeric_std.all;
 
 entity register is
     port (
-        ADDR_1      : IN    STD_LOGIC_VECTOR (25 downto 21);
-        ADDR_2      : IN    STD_LOGIC_VECTOR (20 downto 16);
+        ADDR_1      : IN    STD_LOGIC_VECTOR (4 downto 0);
+        ADDR_2      : IN    STD_LOGIC_VECTOR (4 downto 0);
         ADDR_3      : IN    STD_LOGIC_VECTOR (4 downto 0);
         WR_Data_3   : IN    STD_LOGIC_VECTOR (31 downto 0);
         CLK_in      : IN    STD_LOGIC;
@@ -16,9 +16,20 @@ entity register is
 end entity register;
 
 architecture behavioral of register is
+TYPE ramtype IS ARRAY (31 DOWNTO 0) OF STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL mem: ramtype;
 begin
     process (all)
     begin
+        if (CLK_in(rising) and WR_EN = '0') then
+            RD_Data_1 <= mem(conv_integer((ADDR_1)));
+			RD_Data_2 <= mem(conv_integer((ADDR_2)));
+
         
+
+
+
+
+
     end process;
 end architecture behavioral;
